7713eda74e8d5f4a4a610d354c2f16fac8eab73f
hawser <hawser86@gmail.com>
2025-02-09 14:11:18 +0100

    :recycle: rename variables
    "pins" and "roll" was used for the same thing: the number of pins hit

--- src/bowling-game/BowlingGame.ts
+++ src/bowling-game/BowlingGame.ts
@@ -1,14 +1,14 @@
 export class BowlingGame {
     private rolls: number[] = [];
 
-    public roll(pins: number): void {
-        this.rolls.push(pins);
+    public roll(pinCount: number): void {
+        this.rolls.push(pinCount);
     }
 
     public getScore(): number {
-        return this.rolls.reduce((score, roll, rollIndex) => {
-            const bonus = this.rolls[rollIndex - 1] + roll === 10 ? this.rolls[rollIndex + 1] : 0
-            return score + roll + bonus;
+        return this.rolls.reduce((score, pinCount, rollIndex) => {
+            const bonus = this.rolls[rollIndex - 1] + pinCount === 10 ? this.rolls[rollIndex + 1] : 0
+            return score + pinCount + bonus;
         }, 0);
     }
 }
