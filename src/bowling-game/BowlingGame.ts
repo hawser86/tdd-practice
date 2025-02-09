b90271b4541c19cec3fa8e1ea993cb5bef9fea8a
hawser <hawser86@gmail.com>
2025-02-09 14:23:27 +0100

    :recycle: replace "reduce" with a "for" loop

--- src/bowling-game/BowlingGame.ts
+++ src/bowling-game/BowlingGame.ts
@@ -6,9 +6,14 @@ export class BowlingGame {
     }
 
     public getScore(): number {
-        return this.rolls.reduce((score, pinCount, rollIndex) => {
+        let score = 0;
+
+        for (let rollIndex = 0; rollIndex < this.rolls.length; rollIndex++) {
+            const pinCount = this.rolls[rollIndex];
             const bonus = this.rolls[rollIndex - 1] + pinCount === 10 ? this.rolls[rollIndex + 1] : 0
-            return score + pinCount + bonus;
-        }, 0);
+            score += pinCount + bonus;
+        }
+
+        return score;
     }
 }
