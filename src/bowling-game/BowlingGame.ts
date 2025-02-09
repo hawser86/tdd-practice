20e640a9a72fa05256ae3ed2eed343ddeec03415
hawser <hawser86@gmail.com>
2025-02-09 14:51:14 +0100

    :heavy_check_mark: check frame when calculating strike

--- src/bowling-game/BowlingGame.ts
+++ src/bowling-game/BowlingGame.ts
@@ -11,7 +11,7 @@ export class BowlingGame {
         for (let rollIndex = 0; rollIndex < this.rolls.length; rollIndex++) {
             const pinCount = this.rolls[rollIndex];
             const spareBonus = (rollIndex % 2 !== 0) && (this.rolls[rollIndex - 1] + pinCount === 10) ? this.rolls[rollIndex + 1] : 0
-            const strikeBonus = pinCount === 10 ? this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2] : 0;
+            const strikeBonus = (rollIndex % 2 === 0) && (pinCount === 10) ? this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2] : 0;
             score += pinCount + spareBonus + strikeBonus;
         }
 
