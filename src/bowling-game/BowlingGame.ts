4055eae69e20bc957b32752187b91a04a4919768
hawser <hawser86@gmail.com>
2025-02-09 21:18:30 +0100

    :recycle: refactor score calculation
    extract smaller calculations

--- src/bowling-game/BowlingGame.ts
+++ src/bowling-game/BowlingGame.ts
@@ -10,26 +10,34 @@ export class BowlingGame {
         let frameIndex = 0;
         let rollIndex = 0;
 
-        while (rollIndex < this.rolls.length) {
-            const pinCount = this.rolls[rollIndex];
+        while (rollIndex < this.rolls.length && frameIndex < 10) {
+            const currentRoll = this.rolls[rollIndex];
+            const nextRoll = this.rolls[rollIndex + 1] ?? 0;
+            const secondNextRoll = this.rolls[rollIndex + 2];
 
-            if (pinCount === 10) { // strike
-                score += 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+            if (this.isStrike(rollIndex)) {
+                score += 10 + nextRoll + secondNextRoll;
                 rollIndex += 1;
-            } else if (pinCount + this.rolls[rollIndex + 1] === 10) { // spare
-                score += 10 + this.rolls[rollIndex + 2];
-                rollIndex += 2;
             } else {
-                score += pinCount + (this.rolls[rollIndex + 1] ?? 0);
+                const frameScore = currentRoll + nextRoll;
+                if (this.isSpare(rollIndex)) {
+                    score += secondNextRoll;
+                }
+                score += frameScore;
                 rollIndex += 2;
             }
 
             frameIndex += 1;
-            if (frameIndex >= 10) {
-                break;
-            }
         }
 
         return score;
+    }
+
+    private isStrike(rollIndex: number): boolean {
+        return this.rolls[rollIndex] === 10;
+    }
+
+    private isSpare(rollIndex: number): boolean {
+        return this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10;
     }
 }
