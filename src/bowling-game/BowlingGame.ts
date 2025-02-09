3c73bf23a76812c2dc25ab3cb108a1f81da778e0
hawser <hawser86@gmail.com>
2025-02-09 15:42:42 +0100

    :heavy_check_mark: handle bonus rolls in the last frame

--- src/bowling-game/BowlingGame.ts
+++ src/bowling-game/BowlingGame.ts
@@ -8,13 +8,22 @@ export class BowlingGame {
     public getScore(): number {
         let score = 0;
         let firstRollInFrame = true;
+        let frameIndex = 0;
 
         for (let rollIndex = 0; rollIndex < this.rolls.length; rollIndex++) {
+            if (frameIndex === 10) {
+                break;
+            }
             const pinCount = this.rolls[rollIndex];
             const spareBonus = !firstRollInFrame && (this.rolls[rollIndex - 1] + pinCount === 10) ? this.rolls[rollIndex + 1] : 0
             const strikeBonus = firstRollInFrame && (pinCount === 10) ? this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2] : 0;
+
             score += pinCount + spareBonus + strikeBonus;
+
             firstRollInFrame = pinCount === 10 ? true : !firstRollInFrame;
+            if (firstRollInFrame) {
+                frameIndex++;
+            }
         }
 
         return score;
