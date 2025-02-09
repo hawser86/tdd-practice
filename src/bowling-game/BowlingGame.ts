3505e89cc506a2087a97b20808440f756e3a9fe9
hawser <hawser86@gmail.com>
2025-02-09 15:13:03 +0100

    :heavy_check_mark: handle frames with single rolls (strike)

--- src/bowling-game/BowlingGame.ts
+++ src/bowling-game/BowlingGame.ts
@@ -7,12 +7,14 @@ export class BowlingGame {
 
     public getScore(): number {
         let score = 0;
+        let firstRollInFrame = true;
 
         for (let rollIndex = 0; rollIndex < this.rolls.length; rollIndex++) {
             const pinCount = this.rolls[rollIndex];
-            const spareBonus = (rollIndex % 2 !== 0) && (this.rolls[rollIndex - 1] + pinCount === 10) ? this.rolls[rollIndex + 1] : 0
-            const strikeBonus = (rollIndex % 2 === 0) && (pinCount === 10) ? this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2] : 0;
+            const spareBonus = !firstRollInFrame && (this.rolls[rollIndex - 1] + pinCount === 10) ? this.rolls[rollIndex + 1] : 0
+            const strikeBonus = firstRollInFrame && (pinCount === 10) ? this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2] : 0;
             score += pinCount + spareBonus + strikeBonus;
+            firstRollInFrame = pinCount === 10 ? true : !firstRollInFrame;
         }
 
         return score;
